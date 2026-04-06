7c60adb8f0d73ddf5e8bf6c8b511e5a71be845ca
udaymanhas9 <uday1903@yahoo.com>
2026-04-06 12:35:23 +0100

    Error to do with buffer

--- app/bereal/page.tsx
+++ app/bereal/page.tsx
@@ -555,7 +555,9 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64  = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const raw     = atob(base64);
-  return Uint8Array.from({ length: raw.length }, (_, i) => raw.charCodeAt(i));
+  const arr     = new Uint8Array(raw.length);
+  for (let i = 0; i < raw.length; i++) arr[i] = raw.charCodeAt(i);
+  return arr;
 }
 
 function CameraIcon() {
